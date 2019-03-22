dd8a2282629698652c35832c862913207d7168fe
leonardo.ferreira <leonardo.ferreira@sicoob.com.br>
2019-03-22 19:11:50 -0300

    inclusão do material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CompartilhadoModule } from './modulos-compartilhados/compartilhado.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, CompartilhadoModule],
   providers: [],
   bootstrap: [AppComponent]
 })
