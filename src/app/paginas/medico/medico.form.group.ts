c3797a720638692f75fbd6213dde14d702daa5c4
leonardo <leonardosoares.sistemas@gmail.com>
2019-04-28 18:09:37 -0300

    ajuste

--- src/app/paginas/medico/medico.form.group.ts
+++ src/app/paginas/medico/medico.form.group.ts
@@ -12,40 +12,40 @@ export class MedicoFormGroup {
     montarForGroup() {
         this.form = this.fb.group({
             id: new FormControl(),
-            nome: new FormControl(Validators.required),
-            conselhoMedicina: new FormControl(Validators.required),
-            numeroConselho: new FormControl(Validators.required),
-            ufConselho: new FormControl(Validators.required),
-            cpf: new FormControl(Validators.required),
-            numeroRG: new FormControl(Validators.required),
-            dataEmissaoRG: new FormControl(Validators.required),
-            orgaoEmissor: new FormControl(Validators.required),
-            dataNascimento: new FormControl(Validators.required),
-            email: new FormControl([Validators.required, Validators.email]),
+            nome: new FormControl('', Validators.required),
+            conselhoMedicina: new FormControl('', Validators.required),
+            numeroConselho: new FormControl('', Validators.required),
+            ufConselho: new FormControl('', Validators.required),
+            cpf: new FormControl('', Validators.required),
+            numeroRG: new FormControl('', Validators.required),
+            dataEmissaoRG: new FormControl('', Validators.required),
+            orgaoEmissor: new FormControl('', Validators.required),
+            dataNascimento: new FormControl('', Validators.required),
+            email: new FormControl('', [Validators.required, Validators.email]),
             estadoCivil: new FormGroup({
-                id: new FormControl(Validators.required),
+                id: new FormControl('', Validators.required),
                 descricao: new FormControl('')
             }),
-            naturalidade: new FormControl(Validators.required),
-            nacionalidade: new FormControl(Validators.required),
-            nomeMae: new FormControl(Validators.required),
-            nomePai: new FormControl(Validators.required),
-            sexo: new FormControl(Validators.required),
+            naturalidade: new FormControl('', Validators.required),
+            nacionalidade: new FormControl('', Validators.required),
+            nomeMae: new FormControl('', Validators.required),
+            nomePai: new FormControl('', Validators.required),
+            sexo: new FormControl('', Validators.required),
             endereco: new FormGroup({
-                logradouro: new FormControl(Validators.required),
-                numero: new FormControl(Validators.required),
-                complemento: new FormControl(Validators.required),
-                bairro: new FormControl(Validators.required),
-                cidade: new FormControl(Validators.required),
-                cep: new FormControl(Validators.required),
-                uf: new FormControl(Validators.required)
+                logradouro: new FormControl('', Validators.required),
+                numero: new FormControl('', Validators.required),
+                complemento: new FormControl('', Validators.required),
+                bairro: new FormControl('', Validators.required),
+                cidade: new FormControl('', Validators.required),
+                cep: new FormControl('', Validators.required),
+                uf: new FormControl('', Validators.required)
 
             }),
             telefones: this.fb.array([
                 new FormGroup({
-                    numero: new FormControl(Validators.required),
+                    numero: new FormControl('', Validators.required),
                     tipoTelefone: new FormGroup({
-                        id: new FormControl(Validators.required),
+                        id: new FormControl('', Validators.required),
                         descricao: new FormControl('')
                     })
                 }),
