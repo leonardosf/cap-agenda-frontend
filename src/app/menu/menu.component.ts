f46f28d87c3372f0472961f99e818e2702a63554
dsamuel32 <diego.samuel.alves@gmail.com>
2019-05-01 18:34:56 -0300

    Carregando menu banco de dados.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpService } from './../servicos/http.service';
+import { Component, OnInit } from '@angular/core';
+import { Menu } from './menu';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: 'menu.component.html',
-  styleUrls: ['menu.component.scss'],
+    selector: 'app-menu',
+    templateUrl: 'menu.component.html',
+    styleUrls: ['menu.component.scss'],
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit {
+
+    public menus: Array<Menu> = [];
+    public home = { nome: 'Home', icone: 'home' };
+
+    constructor(private http: HttpService) {
+        this.menus = [];
+    }
+
+    ngOnInit() {
+        this.http.path = 'menus';
+        this.http.recuperarTodos(resposta => { 
+            this.menus = resposta;
+            this.home = this.menus[0];
+            this.menus.splice(0, 1);
+        }, erro => this.menus = []);
+    }
+
+    sucesso(resposta) {
+        this.menus = resposta;
+        
+    }
+
+    erro(error) {
+        this.menus = [];
+    }
+
+}
---
+++ src/app/menu/menu.ts
@@ -0,0 +1,8 @@
+export class Menu {
+    id: number;
+    nome: string;
+    url: string;
+    icone: string;
+    subMenus: Array<Menu>;
+    pai: boolean;
+}
